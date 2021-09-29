04284ff052ea7bbb26c87d7bbb4f375c7da39006
정재훈 <81303380+nasnaky@users.noreply.github.com>
2021-09-29 22:54:43 +0900

    Add files via upload

---
+++ 출력.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main1(void) {
+	int num = 10;
+	std::cout << "Hello Would" << std::endl;
+	std::cout << "Hello" << "Would" << std::endl;
+	std::cout << num << "" << "A";
+	std::cout << "" << 3.14 << std::endl;	
+
+	return 0;
+}
