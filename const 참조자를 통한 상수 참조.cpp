85e523332869ff7ed816d9f8f8553f9ffbec4c72
정재훈 <81303380+nasnaky@users.noreply.github.com>
2021-09-29 22:50:05 +0900

    Add files via upload

---
+++ const 참조자를 통한 상수 참조.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int rhqgkrl(int& a, int& b) {
+	return a + b;
+}
+
+int main_asdfg(void) {
+	int a = 1;
+	int b = 2;
+	int c = rhqgkrl(a, b);
+
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+	//cout << rhqgkrl(4, 5) << endl; //¿À·ù ³ª¿È;
+
+	return 0;
+}
