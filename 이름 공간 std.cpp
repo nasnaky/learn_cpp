b4c926b133c977d7825db24ea5ff672dc9ad6068
정재훈 <81303380+nasnaky@users.noreply.github.com>
2021-09-29 22:52:59 +0900

    Add files via upload

---
+++ 이름 공간 std.cpp
@@ -0,0 +1,19 @@
+//18
+#include<iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+using namespace std;
+
+int main18(void) {
+	int num = 20;
+	char a[200];
+	cout << "집에 가면 뭐할까?" << endl;
+	cout << "우선 잠을 잘 자겠지" << endl;
+	cout << num << "살에 부사관을 하고 .....  그리고 " << endl;
+	cin >> a;
+
+	return 0;
+}
