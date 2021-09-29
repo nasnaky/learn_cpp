09cd12c539fafb29924cfbc5e51c9e5cfc6a4c13
정재훈 <81303380+nasnaky@users.noreply.github.com>
2021-09-29 22:55:11 +0900

    Add files via upload

---
+++ 프로그램 전체에 영향을 미치는 using의 선언 방법.cpp
@@ -0,0 +1,17 @@
+//17
+#include<iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main17(void) {
+	int num = 20;
+	char a[200];
+	cout << "집에 가면 뭐할까?" << endl;
+	cout << "우선 잠을 잘 자겠지" << endl;
+	cout << num << "살에 부사관을 하고 .....  그리고 " << endl;
+	cin >> a;
+
+	return 0;
+}
